91ea6f7eb61eeacd1b2eeec054d2e1c960a82674
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2024-06-11 17:22:30 +0600

    link preview attempt 3

--- src/app/(routes)/movies/[id]/page.tsx
+++ src/app/(routes)/movies/[id]/page.tsx
@@ -1,7 +1,5 @@
-"use client";
-
-import { useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { GetServerSideProps } from 'next';
+import Head from 'next/head';
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import SearchBar from "../../components/SearchBar";
@@ -9,34 +7,26 @@ import SocialHandle from "../../components/Social-Handle";
 import Body from "./components/Body";
 import Suggestions from "../../components/Suggestions";
 import Details from '../../components/Details';
-import Head from "next/head";
 import useMovie from '../../../../../actions/get-movie';
+import { Movie } from '../../../../../type';
 
-const Movie: React.FC = () => {
-    const searchParams = useSearchParams();
-    const imdbID = searchParams.get("id");
-    const { movie, loading, error } = useMovie(imdbID);
-    const [metaTagsSet, setMetaTagsSet] = useState(false);
+interface MovieProps {
+    imdbID: string;
+    movie: Movie;
+}
 
-    useEffect(() => {
-        if (movie && !metaTagsSet) {
-            setMetaTagsSet(true);
-        }
-    }, [movie, metaTagsSet]);
-
+const MoviePage: React.FC<MovieProps> = ({ imdbID, movie }) => {
     return (
         <>
-            {metaTagsSet && (
-                <Head>
-                    <meta property="og:image" content={movie?.Poster} />
-                    <meta property="og:title" content={movie?.Title} />
-                    <meta property="og:description" content={movie?.Plot} />
-                    <meta name="twitter:card" content="summary_large_image" />
-                    <meta name="twitter:title" content={movie?.Title} />
-                    <meta name="twitter:description" content={movie?.Plot} />
-                    <meta name="twitter:image" content={movie?.Poster} />
-                </Head>
-            )}
+            <Head>
+                <meta property="og:image" content={movie?.Poster} />
+                <meta property="og:title" content={movie?.Title} />
+                <meta property="og:description" content={movie?.Plot} />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:title" content={movie?.Title} />
+                <meta name="twitter:description" content={movie?.Plot} />
+                <meta name="twitter:image" content={movie?.Poster} />
+            </Head>
             <div className='flex flex-col pt-8 w-max sm:w-screen md:w-full lg:w-full justify-center items-center max-w-max'>
                 <div className="flex flex-col justify-center items-center ">
                     <Header />
@@ -54,4 +44,16 @@ const Movie: React.FC = () => {
     );
 };
 
-export default Movie;
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const imdbID = context.query.id as string;
+    const movie = await useMovie(imdbID); // Update this line according to how you fetch your movie data
+
+    return {
+        props: {
+            imdbID,
+            movie,
+        },
+    };
+};
+
+export default MoviePage;
