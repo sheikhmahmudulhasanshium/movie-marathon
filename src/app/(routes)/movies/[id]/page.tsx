2d6dea0ea5c64a4309b66d32cfd1f56562ef0e71
sheikhmahmudulhasanshium <sheikh.mahmudul.hasan.shium.aiub@gmail.com>
2024-06-11 16:56:54 +0600

    link preview attempt 2

--- src/app/(routes)/movies/[id]/page.tsx
+++ src/app/(routes)/movies/[id]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import SearchBar from "../../components/SearchBar";
@@ -14,30 +15,42 @@ import useMovie from '../../../../../actions/get-movie';
 const Movie: React.FC = () => {
     const searchParams = useSearchParams();
     const imdbID = searchParams.get("id");
-    const { movie } = useMovie(imdbID);
+    const { movie, loading, error } = useMovie(imdbID);
+    const [metaTagsSet, setMetaTagsSet] = useState(false);
+
+    useEffect(() => {
+        if (movie && !metaTagsSet) {
+            setMetaTagsSet(true);
+        }
+    }, [movie, metaTagsSet]);
 
     return (
-        <div className='flex flex-col pt-8 w-max sm:w-screen md:w-full lg:w-full justify-center items-center max-w-max'>
-            <Head>
-                {movie && (
-                    <>
-                        <meta property="og:image" content={`${movie.Poster}`} />
-                        <meta name="twitter:card" content={`${movie.Title}`} />
-                    </>
-                )}
-            </Head>
-            <div className="flex flex-col justify-center items-center ">
-                <Header />
+        <>
+            {metaTagsSet && (
+                <Head>
+                    <meta property="og:image" content={movie?.Poster} />
+                    <meta property="og:title" content={movie?.Title} />
+                    <meta property="og:description" content={movie?.Plot} />
+                    <meta name="twitter:card" content="summary_large_image" />
+                    <meta name="twitter:title" content={movie?.Title} />
+                    <meta name="twitter:description" content={movie?.Plot} />
+                    <meta name="twitter:image" content={movie?.Poster} />
+                </Head>
+            )}
+            <div className='flex flex-col pt-8 w-max sm:w-screen md:w-full lg:w-full justify-center items-center max-w-max'>
+                <div className="flex flex-col justify-center items-center ">
+                    <Header />
+                </div>
+                <div className="flex flex-col justify-center items-center ">
+                    <SearchBar />
+                    <Body imdbID={imdbID} />
+                    <Details imdbID={imdbID} movieType={"movie"} />
+                    <SocialHandle />
+                    <Suggestions imdbID={imdbID} />
+                    <Footer />
+                </div>
             </div>
-            <div className="flex flex-col justify-center items-center ">
-                <SearchBar />
-                <Body imdbID={imdbID} />
-                <Details imdbID={imdbID} movieType={"movie"}/>
-                <SocialHandle />
-                <Suggestions imdbID={imdbID} />
-                <Footer />
-            </div>
-        </div>
+        </>
     );
 };
 
